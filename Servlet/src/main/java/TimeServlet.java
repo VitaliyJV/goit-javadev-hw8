f4a368795f43af58fe5d50f29b043148e3108467
VitaliyJV <114652130+VitaliyJV@users.noreply.github.com>
2023-08-02 16:25:46 +0300

    1
    1

--- Servlet/src/main/java/TimeServlet.java
+++ Servlet/src/main/java/TimeServlet.java
@@ -1,4 +1,3 @@
-
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -6,56 +5,31 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
 
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) {
-        try {
-            resp.getWriter()
-                    .write(
-                            DateTimeFormatter
-                                    .ofPattern("yyyy-MM-dd HH:mm:ss 'UTC")
-                                    .format(
-                                            LocalDateTime.now().atZone(ZoneId.of(req.getParameter("timezone"))
-                                            )));
-        } catch (Exception e) {
-            resp.setStatus(400);
-            try {
-                resp.getWriter().write("Invalid timezone");
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-        try {
-            resp.getWriter().close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private static final String DEFAULT_TIME_ZONE = "UTC";
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html; charset=utf-8");
+
+        String userTimeZone = req.getParameter("timezone");
+        String formattedDateTime = getDateTimeFromUserQuery(userTimeZone);
+
+        resp.getWriter().write(formattedDateTime);
     }
 
+    private String getDateTimeFromUserQuery(String userTimeZone) {
+        LocalDateTime currentDateTime = LocalDateTime.now(TimeUtils.getTimeZone(userTimeZone));
+        return currentDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " " + userTimeZone;
+    }
 }
 
 /*
-Завдання №1 - напиши сервлет, який віддає HTML сторінку з поточним часом по UTC
-
-Напиши сервлет TimeServlet. Він має обробляти GET запит за адресою /time і віддавати HTML сторінку з поточним часом по
-часовому поясу UTC.
-
-Сторінка має виводити час (з точністю до секунд) та часовий пояс. Наприклад, 2022-01-05 12:05:01 UTC
-Оскільки це GET запит, то його можна протестувати в браузері. Запусти програму, і переконайсь, що вона коректно працює і
-відкривається у браузері. Ти маєш ввести адресу на кшталт http://localhost:8080/time в браузері, і отримати результат.
+http://localhost:8080
+http://localhost:8080/time
 http://localhost:8080/Servlet/time
-
-
-Завдання №2 - розшир сервлет, щоб він приймав часовий пояс
-Розшир сервлет з попереднього завдання, щоб він приймав один query параметр timezone, і повертав час у переданому часовому поясі.
-
-Наприклад, якщо відкрити в браузері URL виду http://localhost:8080/time?timezone=UTC+2, то отримаємо результат на кшталт - 2022-01-05 12:05:01 UTC+2.
-
-Якщо не передавати параметр timezone, то має повертатись час по UTC.
-*/
+ */
--- Servlet/src/main/java/TimeUtils.java
+++ Servlet/src/main/java/TimeUtils.java
@@ -1,6 +1,22 @@
 import java.time.ZoneId;
 
 public class TimeUtils {
+
+    public static ZoneId getTimeZone(String timeZone) {
+        if (timeZone == null || timeZone.isEmpty()) {
+            return null;
+        }
+
+        ZoneId zoneId = null;
+        timeZone = timeZone.replace(" ", "+");
+        try {
+            zoneId = ZoneId.of(timeZone);
+        } catch (RuntimeException e) {
+            throw new RuntimeException();
+        }
+        return zoneId;
+    }
+
     public static boolean validateTimeZone(String timeZone) {
         if (timeZone == null || timeZone.isEmpty()) {
             return false;
