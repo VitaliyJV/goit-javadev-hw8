39e521f999c7ef3c146b15f3ceed72ec0ca08076
VitaliyJV <114652130+VitaliyJV@users.noreply.github.com>
2023-08-03 09:43:33 +0300

    Update TimeServlet.java

--- Servlet/src/main/java/TimeServlet.java
+++ Servlet/src/main/java/TimeServlet.java
@@ -1,4 +1,4 @@
-/*import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
