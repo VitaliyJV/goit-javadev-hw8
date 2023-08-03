f6d4d36250b65ce2d77e69a20a324f67e9850398
VitaliyJV <114652130+VitaliyJV@users.noreply.github.com>
2023-08-03 09:44:32 +0300

    Update TimeServlet1.java

--- Servlet/src/main/java/TimeServlet1.java
+++ Servlet/src/main/java/TimeServlet1.java
@@ -1,6 +1,5 @@
 import jakarta.servlet.annotation.WebServlet;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,31 +7,38 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 @WebServlet(value = "/time")
 public class TimeServlet1 extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String timezone = URLEncoder.encode(req.getParameter("timezone"), StandardCharsets.UTF_8);
         try {
+            ZoneId zoneId = ZoneId.of(timezone);
+            LocalDateTime now = LocalDateTime.now(ZoneId.of(timezone));
+            OffsetDateTime offsetDateTime = now.atOffset(zoneId.getRules().getOffset(now));
             resp.getWriter()
                     .write(
                             DateTimeFormatter.ISO_OFFSET_DATE_TIME
-                                    .format(
-                                            LocalDateTime.now(ZoneId.of(
-                                                    URLEncoder.encode(
-                                                            req.getParameter("timezone"), StandardCharsets.UTF_8)
-                                            ))
-                                    )
+                                    .format(offsetDateTime)
                     );
         } catch (Exception e) {
+            log(e.getMessage(),e);
             resp.setStatus(400);
             resp.getWriter().write("Invalid timezone");
         }
         resp.getWriter().close();
     }
 }
+
 /*
+http://localhost:8080
+
+http://localhost:8080/Servlet/time
+
+http://localhost:8080/Servlet/time?timezone=UTC+2
 
  */
